3b5f63e74c5dffb7e231eb247c23034c42a7006d
Dmitriev-Dmitrii <dmitrievdmitrii@mail.com>
2020-11-18 22:23:17 +0300

    menu bug

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ let menuToggle = document.querySelector('.header__mobile-toggle');
 menuToggle.addEventListener("click",showMenu);
 
 let menuList = document.querySelector('.header__menu-list');
-let body = document.querySelector('.body');
+
 
 function showMenu  ()
 {
@@ -15,12 +15,12 @@ menuToggle.classList.toggle('header__mobile-toggle--active');
 
 if (menuToggle.classList.contains("header__mobile-toggle--active"))
 {
-	body.style.cssText = 'overflow: hidden;';
+
 	menuList.classList.add('header__menu-list--active');
 }
 else
 {
-	body.style.cssText = 'overflow: auto;';
+
 	menuList.classList.remove('header__menu-list--active');
 }
 }
